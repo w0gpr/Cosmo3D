6629abdee74c5dcc0cb29cdc6e3f75ce0baacc07
Brandon <w0gpr59@gmail.com>
2021-09-27 14:51:20 -0400

    initial commit functions

---
+++ src/cosmoDistribution.m
@@ -0,0 +1,45 @@
+function [theta,phi] = cosmoDistribution(num)
+%% determine a random distribution of cosmic radiation products
+% alpha is from 0 to pi, where zero is horizontal and pi/2 is 90 degrees or
+% vertical
+rng('shuffle')
+phi = rand(num,1)*2*pi;
+
+% This section takes twice as long computationally compared to below
+if num<100
+    theta = zeros(num,1);
+    n = 0;
+    while n<num
+        temp = pi/2*rand; y = rand;
+        Power = 2.3;  % 2.3
+        if cos(temp)^Power>=y
+            n = n +1;
+            theta(n) = temp;
+        end
+    end
+else
+    N = 4*num;
+    theta = pi/2*rand(N,1);
+    y = rand(N,1);
+
+    i = cos(theta).^2.3>=y;
+    theta = theta(i);
+
+    % This is a check to make sure there are enought points, 
+    % but is usually not needed for less than 1000
+    if length(theta)>num
+        theta = theta(1:num);
+    else
+        disp('Alert!')
+        N = 10*num;
+        theta = pi*rand(N,1);
+        y = rand(N,1);
+
+        i = sin(theta).^2.3>=y;
+        theta = theta(i);
+        if length(theta)<num
+            error('x is too short')
+        end
+    end
+end
+end
---
+++ src/surfaceModelSingle.m
@@ -0,0 +1,33 @@
+function [X1,Y1,Z1] = surfaceModelSingle(abrasionDepth,xP,zP,xPreset,zPreset)
+
+%% Surface model builder
+% This script builds the surface models that are then output to the main
+% program to be read into the ray trace model. There are three initial end
+% members, and then eventually intermediate scenarios to solve for the
+% minimized chi^2 reduction. 
+
+buf = 300;  % This is 3 meters of buffer on either side of the sample.
+y1 = [-buf-1 -buf buf buf+1];
+
+% convert from percentage to a usable range in the model
+xP = xPreset(2)+(xPreset(3)-xPreset(2))*xP; 
+zP = zPreset(2)+(zPreset(3)-zPreset(2))*zP;
+
+x = linspace(xPreset(2)+1,xPreset(3)-1,25);
+% z = linspace(zPreset(3),zPreset(2),zP);
+
+% [xx,zz] = meshgrid(x,z);
+
+% runs = numel(xx)
+% x1 = [-1 0 481 xx(i) 821 1126 1127];
+% z1 = [0 100 100 zz(i) 220 220 0];
+x1 = [-1 xPreset(1:2) xP xPreset(3:end) xPreset(end)+1];
+z1 = [0 zPreset(1:2) zP zPreset(3:end) 0];
+
+xq = [x1(1:3) x x1(5:end)];
+zq = pchip(x1,z1,xq);
+% z = [z1(1:3) zq z1(5:end)];
+
+[X1,Y1] = meshgrid(xq,y1);
+Z1 = zeros(size(X1));
+Z1(2:3,:) = [zq + abrasionDepth; zq + abrasionDepth];
